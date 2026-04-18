8dc5aa9e8332cd293055f61bff31ab0e37213fda
Alexy Cruz <ACruz@uvltd.com>
2026-04-18 13:25:45 -0500

    vercel auth provider

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -3,13 +3,16 @@
 import { ReactNode } from 'react';
 import { PreferencesProvider } from '@/context/PreferencesContext';
 import { I18nProvider } from '@/context/I18nContext';
+import { AuthProvider } from '@/context/AuthContext';
 
 export default function ClientProviders({ children }: { children: ReactNode }) {
   return (
-    <PreferencesProvider>
-      <I18nProvider>
-        {children}
-      </I18nProvider>
-    </PreferencesProvider>
+    <AuthProvider>
+      <PreferencesProvider>
+        <I18nProvider>
+          {children}
+        </I18nProvider>
+      </PreferencesProvider>
+    </AuthProvider>
   );
 }
